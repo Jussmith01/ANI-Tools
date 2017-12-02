bb86b85e3bdd665c09e942dbd3dc8e2bede392e1
Justin <jsmith48@ufl.edu>
2017-12-02 13:21:34 -0500

    Update cluster gen

--- activelearning/mdal/clst-md-al.py
+++ activelearning/mdal/clst-md-al.py
@@ -16,7 +16,7 @@ import time
 
 #--------------Parameters------------------
 # dir
-dstore = '/home/jujuman/Research/Cluster_AL/water/'
+dstore = '/home/jujuman/Research/cluster_testing/clusters/'
 
 # Molecule file
 molfile = dstore + 'watercluster'
@@ -30,51 +30,54 @@ trajfile = dstore + 'traj.dat'
 # Optimized structure out
 optfile = dstore + 'optmol.xyz'
 
-wkdircv = '/home/jujuman/Research/DataReductionMethods/model6r/model-gdb_r06_comb09_1/cv5/'
-cnstfilecv = wkdircv + 'rHCNO-4.6A_16-3.1A_a4-8.params'
-saefilecv  = wkdircv + 'sae_6-31gd.dat'
+wkdircv = '/home/jujuman/Research/DataReductionMethods/al_working_network/ANI-AL-0707.0000.0411/'
+cnstfilecv = wkdircv + 'train0/rHCNOSFCl-4.6A_16-3.1A_a4-8.params'
+saefilecv  = wkdircv + 'train0/sae_wb97x-631gd.dat'
 nnfprefix  = wkdircv + 'train'
 
 At = ['C', 'O', 'N', 'S', 'F', 'Cl'] # Hydrogens added after check
 
-N = 20
-T = 300.0
+T = 600.0
 L = 30.0
 V = 0.04
-dt = 0.1
-Nm = 902
+dt = 0.25
+#Nm = 902
+Nm = 800
 Nr = 50
 
 Ni = 5
-Ns = 1
+Ns = 100
 
 
-mfile1 = '/home/jujuman/Research/GDB-11-AL-wB97x631gd/gdb11_s01/water_input/gdb11_s01-2.ipt'
+solv_file = '/home/jujuman/Research/cluster_testing/solvents/gdb11_s01-2.ipt'
+solu_dirs = '/home/jujuman/Research/GDB-11-AL-wB97x631gd/aa_chains/aa1/inputs/'
+
+
+gcmddict = {'edgepad': 0.8,
+            'mindist': 1.6,
+            'maxsig' : 0.7,
+            'Nr': Nr,
+            'Nm': Nm,
+            'Ni': Ni,
+            'Ns': Ns,
+            'dt': dt,
+            'V': V,
+            'L': L,
+            'T': T,
+            'Nembed' : 3,
+            'molfile' : molfile,
+            'dstore' : dstore,
+            }
 
 #-------------------------------------------
 
-mol = [hdn.read_rcdb_coordsandnm(mfile)]
+#print(solu)
 
-dgen = pmf.clustergenerator(cnstfilecv, saefilecv, nnfprefix, 5, mol)
+solv = [hdn.read_rcdb_coordsandnm(solv_file)]
+solu = [hdn.read_rcdb_coordsandnm(solu_dirs+f) for f in os.listdir(solu_dirs)]
 
-dgen.edgepad = 0.8
-dgen.mindist = 1.6
+dgen = pmf.clustergenerator(cnstfilecv, saefilecv, nnfprefix, 5, solv, solu)
 
-difo = open(dstore + 'info_data_mddimer.nfo', 'w')
-difo.write('Beginning dimer generation...\n')
+dgen.generate_clusters(gcmddict)
 
-Nt = 0
-Nd = 0
-for i in range(Nr):
-    dgen.init_dynamics(Nm, V, L, dt, T)
 
-    for j in range(Ns):
-        dgen.run_dynamics(Ni, xyzfile, trajfile)
-        dgen.__fragmentbox__(molfile+str(i).zfill(4) + '-' + str(j).zfill(4) + '_')
-        print('Step (',i,',',j,') [', str(dgen.Nd), '/', str(dgen.Nt),'] generated ',len(dgen.frag_list), 'dimers...')
-        difo.write('Step (' + str(i) + ',' + str(i) + ') [' + str(dgen.Nd) + '/' + str(dgen.Nt) + '] generated ' + str(len(dgen.frag_list)) + 'dimers...\n')
-        Nt += dgen.Nt
-        Nd += dgen.Nd
-
-difo.write('Generated '+str(Nd)+' of '+str(Nt)+' tested dimers. Percent: ' + "{:.2f}".format(100.0*Nd/float(Nt)))
-difo.close()
